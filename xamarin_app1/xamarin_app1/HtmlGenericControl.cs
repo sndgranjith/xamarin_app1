73ae1b8329a22205d7090b25ae2b296f420cfd0f
Ranjith D <ranjithkumar_d@preludesys.com>
2020-06-29 21:55:44 +0530

    Add project files.

---
+++ xamarin_app1/xamarin_app1/HtmlGenericControl.cs
@@ -0,0 +1,14 @@
+﻿namespace xamarin_app1
+{
+    internal class HtmlGenericControl
+    {
+        private string v;
+
+        public HtmlGenericControl(string v)
+        {
+            this.v = v;
+        }
+
+        public string InnerHtml { get; set; }
+    }
+}
